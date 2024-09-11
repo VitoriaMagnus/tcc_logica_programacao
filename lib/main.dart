38a53809df9851d2fd787aa300806c0ff5037b4b
VitoriaMagnus <vitoria.magnus@rede.ulbra.br>
2024-09-11 14:03:44 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+void main() {
+  String endereco;
+  String bairro;
+  int numero;
+  String cidade;
+  String estado;
+  int telefone;
+
+  endereco = "Av castelo branco";
+  numero = 1589;
+  bairro = "centro";
+  cidade = "Curitiba";
+  estado = "Paraná";
+  telefone = 41996857412;
+
+  print(
+      'Sejam bem-vindos a loja Cuidapet! Em breve teremos um sistema de autoatendimento.');
+
+  print("--------------------------------");
+
+  print("Nosso enredeço - $endereco $numero, $bairro, $cidade - $estado ");
+
+  print("Telefone para contato - $telefone");
+}
