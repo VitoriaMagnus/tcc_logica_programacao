c09dec5b33cbd65b92042a76c1bfa01223395a88
VitoriaMagnus <vitoria.magnus@rede.ulbra.br>
2024-09-17 18:40:35 -0300

    alteracoes solicitadas no tcc fase 4

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,21 @@ void main() {
 
   print("-----------------------");
   print("Prezado(a), $nome. Seja muito bem-vindo(a) à nossa loja.");
+
   print(
       "\nOferecemos em nossa loja produtos e serviços para seu PET. Para venda de produtos, procure o colaborador Junior e, para serviços como banho ou tosa, procure o colaborador Neto. Obrigado e esperamos que tenha uma ótima experiência em nossa loja.");
+
+  print("-----------------------");
+  print("Como deseja continuar? ");
+  print("\n1 - Ver ofertas de Produtos. \n2 - Ver ofertas de Serviços.");
+
+  var menuLeitura = stdin.readLineSync() as String;
+
+  int menu = int.parse(menuLeitura);
+
+  if (menu == 1) {
+    print("Ração Royal Canin Indor 7,5kg com o valor promocional de R\$280,00");
+  } else {
+    print("Banho e tosa na promoção pelo preço do banho R\$54,00");
+  }
 }
